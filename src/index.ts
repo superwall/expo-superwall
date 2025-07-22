447892c779eac6e4ba7c03968bf7cc1abb7e81a9
Makisuo <minetoaster12@gmail.com>
2025-07-22 11:45:15 +0200

    feat: expose external types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 export * from "./CustomPurchaseControllerProvider"
 export * from "./components"
 export * from "./components"
+
 export { default as SuperwallExpoModule } from "./SuperwallExpoModule"
+export type * from "./SuperwallExpoModule.types"
+
 export * from "./SuperwallProvider"
 export * from "./usePlacement"
 export * from "./useSuperwall"
