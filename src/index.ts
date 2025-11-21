8500a064d532372a58682f4c6fb01020221fd8b3
Makisuo <minetoaster12@gmail.com>
2025-11-21 11:38:53 +0100

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,16 @@ export * from "./components"
 
 export { default as SuperwallExpoModule } from "./SuperwallExpoModule"
 export type * from "./SuperwallExpoModule.types"
-export type * from "./SuperwallOptions"
+export type {
+  LoggingOptions,
+  NetworkEnvironment,
+  PartialSuperwallOptions,
+  PaywallOptions,
+  RestoreFailed,
+  SuperwallOptions,
+  TransactionBackgroundView,
+} from "./SuperwallOptions"
+export { DefaultSuperwallOptions } from "./SuperwallOptions"
 
 export * from "./SuperwallProvider"
 export * from "./usePlacement"
