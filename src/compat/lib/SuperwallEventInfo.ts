6a8fea6466cd0b1a66f3e57eadd86c94d62a2024
Makisuo <31933546+Makisuo@users.noreply.github.com>
2026-03-12 23:52:15 +0100

    Fix compat event decoding for paywall preload events (#177)
    * Fix "Unhandled event type in SuperwallEvent.fromJson: paywallWebviewProcessTerminated"
    
    * Update src/compat/lib/SuperwallEventInfo.ts
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    
    * Fix review comments
    
    * Fix compat paywall preload start event
    
    * Review PR 165 and fix conflicts
    
    ---------
    
    Co-authored-by: Guillaume Diallo <guitoof@users.noreply.github.com>
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- src/compat/lib/SuperwallEventInfo.ts
+++ src/compat/lib/SuperwallEventInfo.ts
@@ -75,6 +75,7 @@ export enum EventType {
   paywallProductsLoadComplete = "paywallProductsLoadComplete",
   paywallWebviewProcessTerminated = "paywallWebviewProcessTerminated",
   paywallProductsLoadMissingProducts = "paywallProductsLoadMissingProducts",
+  paywallPreloadStart = "paywallPreloadStart",
   paywallPreloadComplete = "paywallPreloadComplete",
   paywallProductsLoadRetry = "paywallProductsLoadRetry",
   surveyResponse = "surveyResponse",
@@ -266,6 +267,7 @@ export class SuperwallEvent {
           paywallInfo: PaywallInfo.fromJson(json.paywallInfo),
           identifiers: json.identifiers,
         })
+      case EventType.paywallPreloadStart:
       case EventType.paywallPreloadComplete:
         return new SuperwallEvent({
           type: eventType,
